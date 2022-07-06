f248f7111acdee909ebad67b0931d6bf808b2b0b
iaacornus <iaacornus.devel@gmail.com>
2022-07-06 23:43:51 +0800

    fetch data module to organize the data in a readable format

---
+++ src/utils/fetch_scripts.py
@@ -0,0 +1,38 @@
+from io import TextIOWrapper
+from os.path import dirname
+from os import walk
+from json import load
+from typing import NoReturn
+
+
+def fetch_script() -> None | NoReturn:
+    """Fetch the training data from the curated scripts stored in json file."""
+
+    BASE_DIR: str = "/".join(dirname(__file__).split("/")[:-2])
+    PATH: str = f"{BASE_DIR}/training_data"
+    PATH_PERSONALITIES: str = f"{BASE_DIR}/training_data/personalities"
+
+    try:
+        training_data: dict[str, str] = {}
+        scripts: TextIOWrapper
+        with open(f"{PATH}/scripts.json", "r", encoding="utf-8") as scripts:
+            script: dict[str, str] = load(scripts)
+
+        for script_num, lines in script.items():
+            training_data[f"general_script_{script_num}"] = lines
+
+        training_data["general_responses"] = script["phrases"]
+
+        for file in next(walk(PATH_PERSONALITIES))[2]:
+            with open(
+                    f"{PATH_PERSONALITIES}/{file}", "r", encoding="utf-8"
+                ) as personality_script:
+                script: dict[str, str] = load(personality_script)
+                for i in range(len(script)):
+                    training_data[
+                            f"{file.split('.')[0]}_{i}"
+                        ] = script[f"script_{i}"]
+    except (FileNotFoundError, PermissionError, IOError, SystemError):
+        raise SystemExit
+    else:
+        return training_data
