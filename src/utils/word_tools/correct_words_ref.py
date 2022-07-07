55212ee28f0f244d2836d72306ebfed8e2a436d9
iaacornus <iaacornus.devel@gmail.com>
2022-07-07 20:57:55 +0800

    generator that fetches the words in database

---
+++ src/utils/word_tools/correct_words_ref.py
@@ -0,0 +1,22 @@
+from io import TextIOWrapper
+from os.path import dirname
+from typing import Generator
+from json import load
+
+
+def correct_words_ref() -> Generator:
+    """Fetch the correct words retrived from dictionary.
+
+    Returns a generator.
+    """
+
+    BASE_DIR: str = "/".join(dirname(__file__).split("/")[:-3])
+    PATH: str = f"{BASE_DIR}/database/word_database.json"
+
+    word_db_: TextIOWrapper
+    with open(PATH, "r", encoding="utf-8") as word_db_:
+        word_db: dict[str, list[str]] = load(word_db_)
+
+    word: str
+    for word in word_db.values():
+        yield word.lower()
