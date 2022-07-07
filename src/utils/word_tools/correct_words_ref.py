7b48dfe3919778b793e87ca412ee23b321695c56
iaacornus <iaacornus.devel@gmail.com>
2022-07-07 21:03:36 +0800

    do the operation in try-except-else block

--- src/utils/word_tools/correct_words_ref.py
+++ src/utils/word_tools/correct_words_ref.py
@@ -4,7 +4,7 @@ from typing import Generator
 from json import load
 
 
-def correct_words_ref() -> Generator:
+def correct_words_ref(log: object) -> Generator:
     """Fetch the correct words retrived from dictionary.
 
     Returns a generator.
@@ -13,10 +13,19 @@ def correct_words_ref() -> Generator:
     BASE_DIR: str = "/".join(dirname(__file__).split("/")[:-3])
     PATH: str = f"{BASE_DIR}/database/word_database.json"
 
-    word_db_: TextIOWrapper
-    with open(PATH, "r", encoding="utf-8") as word_db_:
-        word_db: dict[str, list[str]] = load(word_db_)
-
-    word: str
-    for word in word_db.values():
-        yield word.lower()
+    try:
+        word_db_: TextIOWrapper
+        with open(PATH, "r", encoding="utf-8") as word_db_:
+            word_db: dict[str, list[str]] = load(word_db_)
+    except (
+            FileNotFoundError,
+            IOError,
+            PermissionError,
+            SystemExit
+        ) as exception:
+        log.logger("error", f"Exception: {exception} raised, aborting ...")
+        raise SystemExit
+    else:
+        word: str
+        for word in word_db.values():
+            yield word.lower()
